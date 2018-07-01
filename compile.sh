8e1e43810387ead36c05a444696040bdc8eb91df
dady8889 <dady8889@gmail.com>
2018-07-01 13:56:17 +0200

    Fix installation for newest VMware Tools

--- compile.sh
+++ compile.sh
@@ -14,7 +14,7 @@ if hash vmware-uninstall-tools.pl >/dev/null 2>&1; then
 fi
 
 
-VMWARE_INSTALL_OPTIONS="--clobber-kernel-modules=pvscsi,vmblock,vmci,vmhgfs,vmmemctl,vmsync,vmxnet,vmxnet3,vsock"
+VMWARE_INSTALL_OPTIONS=""
 
 if [[ -n "$1" ]]; then
 	VMWARE_INSTALL_OPTIONS="$1"
@@ -25,6 +25,8 @@ pushd vmware-tools-distrib >/dev/null
 if hash systemctl >/dev/null 2>&1; then
   echo "Creating empty init dirs for backwards compatibility"
   for x in {0..6}; do mkdir -p /etc/init.d/rc${x}.d; done
+  sudo cp $SCRIPT_DIR/patches/vmware-tools-thinprint.service /etc/systemd/system/
+  sudo systemctl enable vmware-tools-thinprint.service
   sudo cp $SCRIPT_DIR/patches/vmware-tools.service /etc/systemd/system/
   sudo systemctl enable vmware-tools.service
   echo "Added and enabled VMware Tools systemd service"
@@ -34,6 +36,7 @@ if sudo ./vmware-install.pl --help 2>&1 | grep -q 'force-install'; then
     VMWARE_INSTALL_OPTIONS="--force-install ${VMWARE_INSTALL_OPTIONS}"
 fi
 
+echo "Installing VMware Tools with arguments $VMWARE_INSTALL_OPTIONS"
 sudo ./vmware-install.pl --default ${VMWARE_INSTALL_OPTIONS}
 
 popd >/dev/null
